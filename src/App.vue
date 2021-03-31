ce530bc49cd68adbab2f22c8cf8eb625a0a54e03
ymwang <wangyanming@agree.com.cn>
2021-03-31 20:27:48 +0800

    编写Icon图标组件

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <template>
-  <Avatar url='http://mdrs.yuanjin.tech/FgMwAPYq17So9nwVH44ltDHo7u3c' :size='300' />
+  <div>
+    <Avatar url='http://mdrs.yuanjin.tech/FgMwAPYq17So9nwVH44ltDHo7u3c' :size='300' />
+    <Icon type="home"/>
+    <Icon type="success"/>
+    <Icon type="error"/>
+    <Icon type="close"/>
+    <Icon type="info"/>
+    <Icon type="blog"/>
+    <Icon type="code"/>
+  </div>
 </template>
 
 <script>
 import Avatar from './components/Avatar'
+import Icon from './components/Icon'
 export default {
   components: {
-    Avatar
+    Avatar,
+    Icon
   }
 }
 </script>
+
+<style scoped>
+.iconfont {
+  font-size: 24px;
+  color: #008c8c;
+}
+</style>
---
+++ src/components/Icon.vue
@@ -0,0 +1,42 @@
+<template>
+  <i class="iconfont" :class="fontClass"></i>
+</template>
+
+<script>
+const classMap = {
+  home: 'iconzhuye',
+  success: 'iconzhengque',
+  error: 'iconcuowu',
+  close: 'iconguanbi',
+  warn: 'iconjinggao',
+  info: 'iconxinxi',
+  blog: 'iconblog',
+  code: 'iconcode',
+  about: 'iconset_about_hov',
+  weixin: 'iconweixin',
+  mail: 'iconemail',
+  github: 'icongithub',
+  qq: 'iconqq',
+  arrowUp: 'iconiconfonticonfonti2copy',
+  arrowDown: 'iconiconfonticonfonti2',
+  empty: 'iconempty',
+  chat: 'iconliuyan'
+}
+export default {
+  props: {
+    type: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    fontClass () {
+      return classMap[this.type]
+    }
+  }
+}
+</script>
+
+<style scoped>
+@import '//at.alicdn.com/t/font_2458209_k56zgynmj9d.css';
+</style>
